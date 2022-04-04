cdc3dc001dd53fb43051ffe9213eae43fc61b8f3
Kevin Okamoto <kevin.okamoto@sap.com>
2022-04-04 14:49:51 -0700

    Adding initial application template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type Item = {
+  id: string;
+  name: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'interview-project';
+  items: Item[] = [];
+
+  ngOnInit(): void {
+      this.items = [{
+        id: '001',
+        name: 'Pencil',
+        price: 0.5
+      }, {
+        id: '002',
+        name: 'Eraser',
+        price: 0.4
+      }]
+  }
 }
